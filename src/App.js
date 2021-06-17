a937eca759b6b17d668575d75536887877450a67
Andrew <zebelian.exceedteam@gmail.com>
2021-06-17 15:38:01 +0300

    added todo-item wrapper

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import TodoItem from './components/TodoItem';
+
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        text: 'ahaha'
+    };
   };
 
   render() {
     return (
-        <div className="App"></div>
+        <div className="App">
+            <div className="container">
+                <TodoItem text={this.state.text} />
+            </div>
+        </div>
     );
   }
 }

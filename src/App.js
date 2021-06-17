32bf3c6806e9b627c2e8a6548f5b90ae8ae46144
Andrew <zebelian.exceedteam@gmail.com>
2021-06-17 18:32:07 +0300

    fix potencial merge confllict (App.js)

--- src/App.js
+++ src/App.js
@@ -4,40 +4,40 @@ import TodoItem from './components/TodoItem';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        data: [
-            {text: 'Random text', id: 0},
-            {text: 'One more random text', id: 1}
-        ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {text: 'Random text', id: 0},
+                {text: 'One more random text', id: 1}
+            ]
+        };
     };
-  };
 
-  handleEnterPress = (text) => {
-      const id = this.state.data.length;
-      this.setState(state => ({
-          data: [...state.data, {id, text, checked: false}]
-      }));
+    handleEnterPress = (text) => {
+        const id = this.state.data.length;
+        this.setState(state => ({
+            data: [...state.data, {id, text, checked: false}]
+        }));
     }
 
-  handleRemoveTodo = (id) => {
-      this.setState(state => ({
-          data: state.data.filter(e => e.id !== id)
-      }));
-  }
+    handleRemoveTodo = (id) => {
+        this.setState(state => ({
+            data: state.data.filter(e => e.id !== id)
+        }));
+    }
 
-  render() {
-    return (
-        <div className="App">
-            <div className="container">
-                <h1>todos</h1>
-                <InputField onEnterClick={this.handleEnterPress} />
-                {this.state.data.map(e => <TodoItem onRemoveTodo={this.handleRemoveTodo} text={e.text} id={e.id} />) }
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <h1>todos</h1>
+                    <InputField onEnterClick={this.handleEnterPress}/>
+                    {this.state.data.map(e => <TodoItem onRemoveTodo={this.handleRemoveTodo} text={e.text} id={e.id}/>)}
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;

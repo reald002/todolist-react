41ec50e1214b2a114a8ed00217f01c8a9f2165a3
Andrew <zebelian.exceedteam@gmail.com>
2021-06-18 10:52:06 +0300

    change 'App.js'

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -10,13 +10,14 @@ class Filters extends Component {
         return (
             <div className="filters">
                 <span className='items-left'>
-                    {this.props.itemsCount > 0 ? `${this.props.itemsCount} item${this.props.itemsCount > 1 ? 's' : ''} left` : ''}
+                    {this.props.data.length > 0 ? `${this.props.data.length} item${this.props.data.length > 1 ? 's' : ''} left` : ''}
                 </span>
                 <div className="filter-buttons">
                     <button className="filter-btn">All</button>
                     <button className="filter-btn">Active</button>
                     <button className="filter-btn">Completed</button>
                 </div>
+                <span className="clear-completed-btn">{this.props.data.filter(e => e.checked === true).length > 0 ? 'Clear completed' : ''}</span>
             </div>
         );
     }
